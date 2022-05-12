81a61a8a0d010c121b02d0181fa827908954c9ea
Chuck Breter <chuckb0384@aim.com>
2022-05-12 18:08:30 -0400

    counter

---
+++ w1/d5/counter/server.py
@@ -0,0 +1,33 @@
+from flask import Flask,render_template, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+
+@app.route('/')
+def count_click():
+        if 'count' in session:
+                session["count"] += 1
+        else:
+                session["count"] = 1
+        
+        return render_template("index.html")
+
+@app.route('/twosessions')
+def count_click_two():
+        if 'count' in session:
+                session["count"] += 1
+        else:
+                session["count"] = 1
+        
+        return redirect('/')
+
+@app.route('/destroy_session')
+def destroy_session():
+        session.clear()
+        
+        return redirect('/')
+
+
+
+if __name__ == "__main__":
+        app.run(debug=True)
